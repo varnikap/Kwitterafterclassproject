925cd9b531192431835e28f28962758c9a9130c3
varnikap <78656240+varnikap@users.noreply.github.com>
2021-08-09 22:29:53 -0400

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,1 +1,40 @@
-//For Uploading File
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDFMQPmzRWbyo1Qk5cM8goHoVQR82qtxVI",
+    authDomain: "kwitterdata-63a0c.firebaseapp.com",
+    projectId: "kwitterdata-63a0c",
+    storageBucket: "kwitterdata-63a0c.appspot.com",
+    messagingSenderId: "122903433457",
+    appId: "1:122903433457:web:16ce07d2923a53c4ead950"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+  //<div class= "container"  style=" height: 500px; width:800px; background:#99CCFF; ">
+          //<div id="infoarea">
+                    //<h1> Kwitter</h1>
+                    //<h3> The Online Chating Surface</h3>
+                   // <h4> See What The World Is Interacting About</h4>
+                   //<img src="https://i.pinimg.com/originals/03/62/be/0362bebcb3397519f5bc54fd3ae07094.jpg" width="150px" height="150">
+            //</div>
+            //<hr>
+            //<div id="loginarea">
+                //<h4>Sign In Or Up</h4>
+               // <br>
+                //<input type="text" id="user_name" class="form_control" placeholder="User Name" width="800px">
+               // <br>
+               // <br>
+                //<button id="login_button" class="btn btn_primary" onclick="addUser()">Login To Kwitter</button>
+                // <br>
+
+               //  <br>
+           // </div>
+               
+
+           // </div>
